b924a395ba68b35cd7e5f9f3b42e9e558b36bae1
MartinGO11 <ogutierrez.m94@gmail.com>
2021-01-07 18:59:55 +0000

    done

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-let pronoun = ['the','our'];
-let adj = ['great', 'big' ];
-let noun = ['jogger','racoon'];
-let dom = ['.com', '.net', '.io', '.cl'];
 
-function generatorDomain (){
-    for (let i = 0; i < pronoun.length; i++){
-        for (let j = 0; j < adj.length; j++){
-            for(let k = 0; k < noun.length; k++){
-                for(let l = 0; l < dom.length; l++){
-                    document.write( `<h4> www.${pronoun[i]}${adj[j]}${noun[k]}${dom[l]} </h4> `);
-                    document.write("<br>");
-                }
+let pron = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let dom = ['.com']
+/*se declaran funciones de cada paso*/
+
+var arraypronAndAdjAndNoun = [];
+function domainNameGenerator() {
+
+     for (i = 0; i < pron.length; i++)/*comienzo con el primer pronombre*/ {
+
+        for (j = 0; j < adj.length; j++)/* se junta el pronombre con cada adjetivo */ {
+
+
+            for (k = 0; k < noun.length; k++) {
+                arraypronAndAdjAndNoun.push(pron[i] + adj[j] + noun[k] + ".com");
             }
+
         }
     }
+    console.log(arraypronAndAdjAndNoun);
 }
-generatorDomain();
+
+domainNameGenerator();
